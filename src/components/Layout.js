26f959de5e9a19380df891f88de00abdcecae064
Abhith <dearabhith@gmail.com>
2019-04-19 21:51:37 +0400

    template theme files removed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
-import './all.sass'
+
 import useSiteMetadata from './SiteMetadata'
 
 const TemplateWrapper = ({ children }) => {
