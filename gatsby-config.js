d14d266f8b2837e50d88f4cf7ea3d5c791dc3421
Abhith <dearabhith@gmail.com>
2019-04-21 01:30:00 +0400

    css purging config update

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,7 +71,7 @@ module.exports = {
       resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
       options: {
         develop: true, // Activates purging in npm run develop
-        purgeOnly: ['/main.css', '/theme.css'],
+        // purgeOnly: ['/main.css', '/theme.css'],
       },
     }, // must be after other CSS plugins
     'gatsby-plugin-netlify', // make sure to keep it last in the array
