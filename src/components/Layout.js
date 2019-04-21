40d7286f20a7a2d4eca7f705b44bdd0406e0762d
Abhith <dearabhith@gmail.com>
2019-04-21 21:25:48 +0400

    container updated as per theme

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -45,7 +45,8 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:image" content="/img/og-image.jpg" />
       </Helmet>
       <Navbar />
-      <div>{children}</div>
+      <main role="main" class="site-content">{children}
+      </main>
       <Footer />
     </div>
   )
