a01d373abc9fabb1ab4b7372822e8e309e1d00c5
Abhith <dearabhith@gmail.com>
2019-04-27 13:38:55 +0400

    gatsby-remark-external-links  and react-icons added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,79 +1,80 @@
-var proxy = require("http-proxy-middleware")
+var proxy = require("http-proxy-middleware");
 
 module.exports = {
   siteMetadata: {
-    title: 'Abhith Rajan',
+    title: "Abhith Rajan",
     description:
-      'an aspiring software engineer with more than 6 years of experience and proven successful track record of delivering technology-based products.',
+      "an aspiring software engineer with more than 6 years of experience and proven successful track record of delivering technology-based products."
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sass",
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/static/img`,
-        name: 'uploads',
-      },
+        name: "uploads"
+      }
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/pages`,
-        name: 'pages',
-      },
+        name: "pages"
+      }
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/img`,
-        name: 'images',
-      },
+        name: "images"
+      }
     },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-relative-images',
+            resolve: "gatsby-remark-relative-images",
             options: {
-              name: 'uploads',
-            },
+              name: "uploads"
+            }
           },
           {
-            resolve: 'gatsby-remark-images',
+            resolve: "gatsby-remark-images",
             options: {
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
-              maxWidth: 2048,
-            },
+              maxWidth: 2048
+            }
           },
           {
-            resolve: 'gatsby-remark-copy-linked-files',
+            resolve: "gatsby-remark-copy-linked-files",
             options: {
-              destinationDir: 'static',
-            },
+              destinationDir: "static"
+            }
           },
-        ],
-      },
+          "gatsby-remark-external-links"
+        ]
+      }
     },
     {
-      resolve: 'gatsby-plugin-netlify-cms',
+      resolve: "gatsby-plugin-netlify-cms",
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
+        modulePath: `${__dirname}/src/cms/cms.js`
+      }
     },
     {
-      resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
+      resolve: "gatsby-plugin-purgecss", // purges all unused/unreferenced css rules
       options: {
         develop: true, // Activates purging in npm run develop
-        purgeOnly: [],
+        purgeOnly: []
         // purgeOnly: ['/main.css', '/theme.css'],
-      },
+      }
     }, // must be after other CSS plugins
     {
       resolve: `gatsby-plugin-google-analytics`,
@@ -82,10 +83,10 @@ module.exports = {
         // Puts tracking script in the head instead of the body
         head: true,
         // Setting this parameter is also optional
-        respectDNT: true,        
-      },
+        respectDNT: true
+      }
     },
-    'gatsby-plugin-netlify', // make sure to keep it last in the array
+    "gatsby-plugin-netlify" // make sure to keep it last in the array
   ],
   // for avoiding CORS while developing Netlify Functions locally
   // read more: https://www.gatsbyjs.org/docs/api-proxy/#advanced-proxying
@@ -95,9 +96,9 @@ module.exports = {
       proxy({
         target: "http://localhost:9000",
         pathRewrite: {
-          "/.netlify/functions/": "",
-        },
+          "/.netlify/functions/": ""
+        }
       })
-    )
-  },
-}
+    );
+  }
+};
