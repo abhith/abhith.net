ced080b5363e56c3b24edfa4bf1d24929e9ca327
Abhith <dearabhith@gmail.com>
2019-04-27 18:43:05 +0400

    chore

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { graphql, StaticQuery, Link } from "gatsby";
-import Img from "gatsby-image";
+// import Img from "gatsby-image";
 
 export default function Hero() {
   return (
