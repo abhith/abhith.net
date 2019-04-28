95e2f88af896adc2e3b423b8c5ef85aa77943550
Abhith <dearabhith@gmail.com>
2019-04-28 23:43:47 +0400

    minor correction - extra container removed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,130 +1,54 @@
-import React from 'react'
-// import { Link } from 'gatsby'
-
-// import logo from '../img/logo.svg'
-// import facebook from '../img/social/facebook.svg'
-// import instagram from '../img/social/instagram.svg'
-// import twitter from '../img/social/twitter.svg'
-// import vimeo from '../img/social/vimeo.svg'
+import React from "react";
+import { OutboundLink } from "gatsby-plugin-google-analytics";
 
 const Footer = class extends React.Component {
   render() {
     return (
-      <div class="container mt-5">
-        <footer class="bg-white border-top p-3 text-muted small">
+      <footer class="bg-white border-top p-3 text-muted small">
+        <div class="container">
           <div class="row align-items-center justify-content-between">
             <div>
-              <span class="navbar-brand mr-2"><strong>Abhith</strong></span> Copyright &copy;
-			<script>document.write(new Date().getFullYear())</script>
-              . All rights reserved.
-		</div>
+              <span class="navbar-brand mr-2 mb-0">
+                <strong>Abhith</strong>
+              </span>
+              <span>
+                Copyright ©{" "}
+                <script>document.write(new Date().getFullYear())</script>.
+              </span>
+
+              <OutboundLink
+                className="text-dark ml-1"
+                target="_blank"
+                href="https://github.com/wowthemesnet/mundana-theme-jekyll"
+              >
+                <i class="fab fa-github" /> Fork on Github
+              </OutboundLink>
+            </div>
             <div>
-              Made with <a target="_blank" rel="noopener noreferrer" class="text-secondary font-weight-bold" href="https://www.wowthemes.net/mundana-free-html-bootstrap-template/">Mundana Theme</a> by WowThemes.net.
-		</div>
+              Made with{" "}
+              <OutboundLink
+                target="_blank"
+                className="text-dark font-weight-bold"
+                href="https://www.wowthemes.net/mundana-jekyll-theme/"
+              >
+                {" "}
+                Mundana Jekyll Theme{" "}
+              </OutboundLink>{" "}
+              by{" "}
+              <OutboundLink
+                className="text-dark"
+                target="_blank"
+                href="https://www.wowthemes.net"
+              >
+                WowThemes
+              </OutboundLink>
+              .
+            </div>
           </div>
-        </footer>
-      </div>
-
-      // <footer className="footer has-background-black has-text-white-ter">
-      //   <div className="content has-text-centered">
-      //     <img
-      //       src={logo}
-      //       alt="Kaldi"
-      //       style={{ width: '14em', height: '10em' }}
-      //     />
-      //   </div>
-      //   <div className="content has-text-centered has-background-black has-text-white-ter">
-      //     <div className="container has-background-black has-text-white-ter">
-      //       <div className="columns">
-      //         <div className="column is-4">
-      //           <section className="menu">
-      //             <ul className="menu-list">
-      //               <li>
-      //                 <Link to="/" className="navbar-item">
-      //                   Home
-      //                 </Link>
-      //               </li>
-      //               <li>
-      //                 <Link className="navbar-item" to="/about">
-      //                   About
-      //                 </Link>
-      //               </li>
-      //               <li>
-      //                 <Link className="navbar-item" to="/products">
-      //                   Products
-      //                 </Link>
-      //               </li>
-      //               <li>
-      //                 <Link className="navbar-item" to="/contact/examples">
-      //                   Form Examples
-      //                 </Link>
-      //               </li>
-      //               <li>
-      //                 <a
-      //                   className="navbar-item"
-      //                   href="/admin/"
-      //                   target="_blank"
-      //                   rel="noopener noreferrer"
-      //                 >
-      //                   Admin
-      //                 </a>
-      //               </li>
-      //             </ul>
-      //           </section>
-      //         </div>
-      //         <div className="column is-4">
-      //           <section>
-      //             <ul className="menu-list">
-      //               <li>
-      //                 <Link className="navbar-item" to="/blog">
-      //                   Latest Stories
-      //                 </Link>
-      //               </li>
-      //               <li>
-      //                 <Link className="navbar-item" to="/contact">
-      //                   Contact
-      //                 </Link>
-      //               </li>
-      //             </ul>
-      //           </section>
-      //         </div>
-      //         <div className="column is-4 social">
-      //           <a title="facebook" href="https://facebook.com">
-      //             <img
-      //               src={facebook}
-      //               alt="Facebook"
-      //               style={{ width: '1em', height: '1em' }}
-      //             />
-      //           </a>
-      //           <a title="twitter" href="https://twitter.com">
-      //             <img
-      //               className="fas fa-lg"
-      //               src={twitter}
-      //               alt="Twitter"
-      //               style={{ width: '1em', height: '1em' }}
-      //             />
-      //           </a>
-      //           <a title="instagram" href="https://instagram.com">
-      //             <img
-      //               src={instagram}
-      //               alt="Instagram"
-      //               style={{ width: '1em', height: '1em' }}
-      //             />
-      //           </a>
-      //           <a title="vimeo" href="https://vimeo.com">
-      //             <img
-      //               src={vimeo}
-      //               alt="Vimeo"
-      //               style={{ width: '1em', height: '1em' }}
-      //             />
-      //           </a>
-      //         </div>
-      //       </div>
-      //     </div>
-      //   </div>
-      // </footer>
-    )
+        </div>
+      </footer>
+    );
   }
-}
+};
 
-export default Footer
+export default Footer;
