c8f748eb747dc9cd6e92729ed4b1b69c8c2a5127
Abhith <dearabhith@gmail.com>
2019-05-04 14:04:32 +0400

    chore (cleaned)

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -26,22 +26,6 @@ export const AboutPageTemplate = ({
         <PageContent className="content" content={content} />
       </div>
     </div>
-
-    // <section className="section section--gradient">
-
-    //   <div className="container">
-    //     <div className="columns">
-    //       <div className="column is-10 is-offset-1">
-    //         <div className="section">
-    //           <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-
-    //           </h2>
-
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </section>
   );
 };
 
