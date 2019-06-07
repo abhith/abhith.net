d4faf42b692e35569ed1d0977132d4ef8323b6ed
Abhith <dearabhith@gmail.com>
2019-06-07 22:43:46 +0400

    Tags listing page UI updated with more relevant details

--- src/pages/tags/index.js
+++ src/pages/tags/index.js
@@ -1,45 +1,112 @@
 import React from "react";
-import { kebabCase } from "lodash";
+import { kebabCase, sortBy } from "lodash";
 import { Link, graphql } from "gatsby";
 import Layout from "../../components/Layout";
 import SEO from "../../components/seo/SEO";
+import { FaFileAlt, FaVideo, FaBookOpen } from "react-icons/fa";
 
 const TagsPage = ({
   data: {
-    allMarkdownRemark: { group }
+    allMarkdownRemark: { group: postsGroup },
+    allStoriesJson: { group: storiesGroup },
+    allVideosJson: { group: videosGroup }
   }
-}) => (
-  <Layout>
-    <section className="section">
-      <SEO
-        title="Tags"
-        description="Summary of all the tags on abhith.net"
-        slug="/tags"
-      />
+}) => {
+  let tags = [];
 
-      <div className="container content">
-        <div className="columns">
-          <div
-            className="column is-10 is-offset-1"
-            style={{ marginBottom: "6rem" }}
-          >
-            <h1 className="title is-size-2 is-bold-light">Tags</h1>
-            <ul className="taglist">
-              {group.map(tag => (
-                <li key={tag.fieldValue}>
-                  <Link to={`/tags/${kebabCase(tag.fieldValue)}/`}>
-                    {tag.fieldValue} ({tag.totalCount})
-                  </Link>
-                </li>
+  postsGroup.forEach(node => {
+    tags.push({
+      slug: node.fieldValue,
+      totalPosts: node.totalCount,
+      totalVideos: 0,
+      totalStories: 0
+    });
+  });
+
+  storiesGroup.forEach(node => {
+    let tag = tags.find(tag => tag.slug === node.fieldValue);
+    if (tag) {
+      tag.totalStories = node.totalCount;
+    } else {
+      tags.push({
+        slug: node.fieldValue,
+        totalPosts: 0,
+        totalVideos: 0,
+        totalStories: node.totalCount
+      });
+    }
+  });
+
+  videosGroup.forEach(node => {
+    let tag = tags.find(tag => tag.slug === node.fieldValue);
+    if (tag) {
+      tag.totalVideos = node.totalCount;
+    } else {
+      tags.push({
+        slug: node.fieldValue,
+        totalPosts: 0,
+        totalVideos: node.totalCount,
+        totalStories: 0
+      });
+    }
+  });
+
+  tags = sortBy(tags, tag => tag.slug);
+
+  return (
+    <Layout>
+      <section className="section">
+        <SEO
+          title="Tags"
+          description="Summary of all the tags on abhith.net"
+          slug="/tags"
+        />
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-md-8">
+              <h1 className="font-weight-bold title h6 text-uppercase mb-4">
+                <span>Tags</span>
+              </h1>
+              {tags.map(tag => (
+                <div key={tag.slug} className="mb-3">
+                  <span className="taglist">
+                    <Link
+                      className="sscroll btn btn-dark btn-lg"
+                      to={`/tags/${kebabCase(tag.slug)}/`}
+                    >
+                      {tag.slug}
+                    </Link>
+                    <Link
+                      className="sscroll btn btn-light btn-sm"
+                      to={`/tags/${kebabCase(tag.slug)}/`}
+                    >
+                      <FaFileAlt /> {tag.totalPosts} POSTS
+                    </Link>
+                    <Link
+                      className="sscroll btn btn-lightblue btn-sm"
+                      to={`/tags/${kebabCase(tag.slug)}/`}
+                    >
+                      <FaVideo /> {tag.totalVideos} VIDEOS
+                    </Link>
+                    <Link
+                      className="sscroll btn btn-gray btn-sm"
+                      to={`/tags/${kebabCase(tag.slug)}/`}
+                    >
+                      <FaBookOpen /> {tag.totalStories} STORIES
+                    </Link>
+                  </span>
+                </div>
               ))}
-            </ul>
+            </div>
+            <div className="col-md-4">
+              {/* {% include sidebar-featured.html %}     */}
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-  </Layout>
-);
-
+      </section>
+    </Layout>
+  );
+};
 export default TagsPage;
 
 export const tagPageQuery = graphql`
@@ -49,8 +116,20 @@ export const tagPageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(limit: 1000) {
+    allMarkdownRemark {
       group(field: frontmatter___tags) {
+        fieldValue
+        totalCount
+      }
+    }
+    allStoriesJson {
+      group(field: tags) {
+        fieldValue
+        totalCount
+      }
+    }
+    allVideosJson {
+      group(field: tags) {
         fieldValue
         totalCount
       }
