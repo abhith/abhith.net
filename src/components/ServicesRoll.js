be2d63e1e36b31a7b6fee82aff2c1948f6c32add
Abhith <dearabhith@gmail.com>
2019-06-09 22:19:33 +0400

    Chore (corrections)

--- src/components/ServicesRoll.js
+++ src/components/ServicesRoll.js
@@ -8,33 +8,37 @@ const ServicesRoll = props => {
   const { services } = props;
 
   if (!services || !services.length) {
-    return <span>Services Coming Soon...</span>;
+    return (
+      <div>
+        <span className="text-muted d-block mb-1">Services Coming Soon...</span>
+      </div>
+    );
   }
 
   return (
     <>
       {services &&
         services.map(({ node: service }) => (
-          <div class="col-lg-6 mb-4" key={service.id}>
-            <div class="p-4 border rounded">
-              <div class="row">
-                <div class="col-md-3 mb-4 mb-md-0">
+          <div className="col-lg-6 mb-4" key={service.id}>
+            <div className="p-4 border rounded">
+              <div className="row">
+                <div className="col-md-3 mb-4 mb-md-0">
                   <img
                     alt={service.title}
                     src={`/img/tags/${service.tags[0]}.png`}
-                    class="rounded-circle"
+                    className="rounded-circle"
                     height="80"
                     width="80"
                   />
                 </div>
-                <div class="col-md-9">
+                <div className="col-md-9">
                   <OutboundLink target="_blank" href={service.url}>
-                    <h4 class="text-dark mb-0"> {service.title} </h4>
+                    <h4 className="text-dark mb-0"> {service.title} </h4>
                   </OutboundLink>
                   <small className="d-block text-muted">
                     In <Tags tags={service.tags} />
                   </small>
-                  <div class="excerpt">
+                  <div className="excerpt">
                     {truncate(service.description, { length: 86 })}
                   </div>
                 </div>
