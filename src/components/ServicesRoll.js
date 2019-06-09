0ea8080a2d919937a0067e4419cb6d6cfbe7ee79
Abhith <dearabhith@gmail.com>
2019-06-09 21:34:14 +0400

    recommended services page UI changed

--- src/components/ServicesRoll.js
+++ src/components/ServicesRoll.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { OutboundLink } from "gatsby-plugin-google-analytics";
 import Tags from "./Tags";
+import { truncate } from "lodash";
 
 const ServicesRoll = props => {
   const { services } = props;
@@ -14,30 +15,28 @@ const ServicesRoll = props => {
     <>
       {services &&
         services.map(({ node: service }) => (
-          <div className="col-md-6" key={service.id}>
-            <div className="card border-0 mb-4 box-shadow">
-              <OutboundLink target="_blank" href={service.url}>
-                <div
-                  style={{ backgroundImage: `url(${service.image})` }}
-                  className={`img-bg topfirstimage`}
-                />
-              </OutboundLink>
-
-              <div className="card-body px-0 pb-0 d-flex flex-column align-items-start">
-                <h2 className="h4 font-weight-bold">
-                  <OutboundLink
-                    className="text-dark"
-                    target="_blank"
-                    href={service.url}
-                  >
-                    {service.title}
+          <div class="col-lg-6 mb-4" key={service.id}>
+            <div class="p-4 border rounded">
+              <div class="row">
+                <div class="col-md-3 mb-4 mb-md-0">
+                  <img
+                    alt={service.title}
+                    src={`/img/tags/${service.tags[0]}.png`}
+                    class="rounded-circle"
+                    height="80"
+                    width="80"
+                  />
+                </div>
+                <div class="col-md-9">
+                  <OutboundLink target="_blank" href={service.url}>
+                    <h4 class="text-dark mb-0"> {service.title} </h4>
                   </OutboundLink>
-                </h2>
-                <p className="excerpt">{service.description}</p>
-                <div>
                   <small className="d-block text-muted">
                     In <Tags tags={service.tags} />
                   </small>
+                  <div class="excerpt">
+                    {truncate(service.description, { length: 86 })}
+                  </div>
                 </div>
               </div>
             </div>
--- src/pages/recommended/services/index.js
+++ src/pages/recommended/services/index.js
@@ -17,22 +17,15 @@ export default class RecommendedServicesIndexPage extends React.Component {
             description="Service/Service providers which Abhith recommends."
             slug="\recommended\services"
           />
-          <div className="row mt-3">
-            <div className="col-md-8 main-loop">
-              <h1 className="font-weight-bold title h6 text-uppercase mb-4">
-                <span>Recommended</span>
-              </h1>
-              <h4 className="font-weight-bold spanborder">
-                <span>Services</span>
-              </h4>
-              <div className="row">
-                <ServicesRoll services={services} />
-              </div>
-            </div>
 
-            <div className="col-md-4">
-              {/* {% include sidebar-featured.html %}     */}
-            </div>
+          <h1 className="font-weight-bold title h6 text-uppercase mb-4">
+            <span>Recommended</span>
+          </h1>
+          <h4 className="font-weight-bold spanborder">
+            <span>Services</span>
+          </h4>
+          <div className="row gap-y listrecent listrecent listauthor">
+            <ServicesRoll services={services} />
           </div>
         </div>
       </Layout>
