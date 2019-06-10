f4c86cb308b8488a030300d2171e9f3118390bb7
Abhith <dearabhith@gmail.com>
2019-06-10 23:47:11 +0400

    Chore (clean)

--- src/components/StoriesRoll.js
+++ src/components/StoriesRoll.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { OutboundLink } from "gatsby-plugin-google-analytics";
-// import PreviewCompatibleImage from "./PreviewCompatibleImage";
 import TagImage from "./TagImage";
 
 import Tags from "./Tags";
