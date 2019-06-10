d959b682d8175bc0f76beaf4dc97506148d329d4
Abhith <dearabhith@gmail.com>
2019-06-10 23:45:25 +0400

    module name correction

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,9 +1,9 @@
-import CMS from 'netlify-cms'
+import CMS from "netlify-cms-app";
 
-import AboutPagePreview from './preview-templates/AboutPagePreview'
-import BlogPostPreview from './preview-templates/BlogPostPreview'
-import IndexPagePreview from './preview-templates/IndexPagePreview'
+import AboutPagePreview from "./preview-templates/AboutPagePreview";
+import BlogPostPreview from "./preview-templates/BlogPostPreview";
+import IndexPagePreview from "./preview-templates/IndexPagePreview";
 
-CMS.registerPreviewTemplate('index', IndexPagePreview)
-CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate("index", IndexPagePreview);
+CMS.registerPreviewTemplate("about", AboutPagePreview);
+CMS.registerPreviewTemplate("blog", BlogPostPreview);
