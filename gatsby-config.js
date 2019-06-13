6cc5729163c7a91aef43180255d6ba2b86c00283
Abhith <dearabhith@gmail.com>
2019-06-13 22:54:34 +0400

    css purging disabled in develop

--- gatsby-config.js
+++ gatsby-config.js
@@ -143,7 +143,7 @@ module.exports = {
       resolve: "gatsby-plugin-purgecss", // purges all unused/unreferenced css rules
       options: {
         printRejected: true,
-        develop: true, // Activates purging in npm run develop
+        develop: false, // Activates purging in npm run develop
         purgeOnly: ["/main.css", "/theme.css"] // applies purging only on the bulma css file
       }
     }, // must be after other CSS plugins
