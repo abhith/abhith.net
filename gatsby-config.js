a1e6d87c79c3ca6d75aec83c8e80b408af92a4ef
Abhith <dearabhith@gmail.com>
2019-06-14 19:07:37 +0400

    embedded videos on post enabled

--- gatsby-config.js
+++ gatsby-config.js
@@ -153,6 +153,23 @@ module.exports = {
         trackingId: "UA-104139048-1"
       }
     },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-embed-video",
+            options: {
+              width: 800,
+              ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
+              height: 400, // Optional: Overrides optional.ratio
+              related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
+              noIframeBorder: true //Optional: Disable insertion of <style> border: 0
+            }
+          }
+        ]
+      }
+    },
     "gatsby-plugin-netlify" // make sure to keep it last in the array
   ],
   // for avoiding CORS while developing Netlify Functions locally
