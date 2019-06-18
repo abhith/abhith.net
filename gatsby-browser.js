2c82d63f26af42b321e357c6f62b0d97dacb6001
Abhith <dearabhith@gmail.com>
2019-06-18 21:44:39 +0400

    old template css reference removed

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
-import "./src/styles/main.css";
-import "./src/styles/theme.css";
-import "./src/styles/custom.css";
+// import "./src/styles/main.css";
+// import "./src/styles/theme.css";
+// import "./src/styles/custom.css";
 import "./src/styles/prism-atom-dark.css";
