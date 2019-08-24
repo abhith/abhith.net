54ff8ed93b7a9031ee738bdd7ee08cf3ce0ab7be
Abhith <dearabhith@gmail.com>
2019-08-24 12:46:40 +0400

    redundant netlify proxy removed

--- gatsby-config.js
+++ gatsby-config.js
@@ -166,18 +166,5 @@ module.exports = {
     },
     `gatsby-plugin-offline`,
     "gatsby-plugin-netlify" // make sure to keep it last in the array
-  ],
-  // for avoiding CORS while developing Netlify Functions locally
-  // read more: https://www.gatsbyjs.org/docs/api-proxy/#advanced-proxying
-  developMiddleware: app => {
-    app.use(
-      "/.netlify/functions/",
-      proxy({
-        target: "http://localhost:9000",
-        pathRewrite: {
-          "/.netlify/functions/": ""
-        }
-      })
-    );
-  }
+  ]
 };
