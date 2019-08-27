7ef667ba66a8f44a1630804e97c94fa963e6382d
Abhith <dearabhith@gmail.com>
2019-08-27 22:38:37 +0400

    blog roll theme updated

--- src/components/BlogRollItem.js
+++ src/components/BlogRollItem.js
@@ -2,29 +2,35 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "gatsby";
 import Img from "gatsby-image";
-import TopicsBar from "./TopicsBar";
 
-const BlogRollItem = ({ post: node }) => {
+const BlogRollItem = ({ post }) => {
   return (
-    <div className="columns">
-      <div className="column is-one-third">
-        <figure className="image">
-          <Link to={`${node.fields.slug}`}>
-            <Img fluid={node.frontmatter.image.childImageSharp.fluid} />
-          </Link>
-        </figure>
-      </div>
-      <div className="column">
-        <div className="content">
-          <Link to={`${node.fields.slug}`}>
-            <p className="title is-6">{node.frontmatter.title}</p>
-          </Link>
-          <small className="text-muted">
-            {node.frontmatter.date} &middot; {node.fields.readingTime.text}
-          </small>
-          <TopicsBar topics={node.frontmatter.tags} />
+    <div className="blog-post">
+      <Link to={post.fields.slug}>
+        <div className="featured-image">
+          <Img fluid={post.frontmatter.image.childImageSharp.fluid} />
         </div>
-      </div>
+        <div className="content">
+          <div className="post-title">{post.frontmatter.title}</div>
+          <span className="blog-date">
+            {post.frontmatter.date} &middot; {post.fields.readingTime.text}
+          </span>
+          <p>{post.frontmatter.description}</p>
+          <div className="post-meta">
+            <div className="author-block">
+              <div className="image is-32x32">
+                <img src="/img/abhith-avatar.jpg" alt="abhith rajan" />
+              </div>
+              <div className="author-name">
+                <span>by Abhith Rajan</span>
+                <span>
+                  <small>in</small> {post.frontmatter.tags.join()}
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Link>
     </div>
   );
 };
