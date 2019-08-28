732eac579a66517584808012121d9c606169277e
Abhith <dearabhith@gmail.com>
2019-08-28 20:17:21 +0400

    Recommended section divider added on home

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -14,42 +14,64 @@ export const IndexPageTemplate = ({ stories, featured, videos }) => (
     <SEO />
     <Hero />
     <LatestPosts />
-
+    <div className="hero is-primary is-bold">
+      <div className="hero-body">
+        <div className="container is-fluid">
+          <div className="columns is-centered">
+            <div className="column is-12 is-10-fullhd">
+              <div className="columns is-vcentered">
+                <div className="column">
+                  <div className="title"> 📬 Recommended </div>
+                  <div className="subtitle">
+                    Developer stories, videos and services
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <div className="section">
-      <div className="columns">
-        <div className="column is-one-third">
-          <h4 className="spanborder">
-            <span className="title is-4 has-text-weight-bold">
-              Recommended Stories
-            </span>
-          </h4>
-          <StoriesRoll posts={stories} />
-          <div className="cta-wrapper has-text-centered">
-            <Link to="/recommended/stories" className="button k-button k-primary raised has-gradient is-bold">
-              <span className="text">View More Stories</span>
-              <span className="front-gradient"></span>
-            </Link>
+      <div className="container is-fluid">
+        <div className="columns">
+          <div className="column is-one-third">
+            <h4 className="spanborder">
+              <span className="title is-4 has-text-weight-bold">
+                Recommended Stories
+              </span>
+            </h4>
+            <StoriesRoll posts={stories} />
+            <div className="cta-wrapper has-text-centered">
+              <Link
+                to="/recommended/stories"
+                className="button k-button k-primary raised has-gradient is-bold"
+              >
+                <span className="text">View More Stories</span>
+                <span className="front-gradient"></span>
+              </Link>
+            </div>
           </div>
-        </div>
-        <div className="column is-one-third">
-          <h4 className="spanborder">
-            <span className="title is-4 has-text-weight-bold">
-              Recommended Videos
-            </span>
-          </h4>
-          <VideosRoll videos={videos} />
-          <div className="cta-wrapper has-text-centered">
-            <Link
-              to="/recommended/videos"
-              className="button k-button k-primary raised has-gradient is-bold"
-            >
-              <span className="text">View More Videos</span>
-              <span className="front-gradient"></span>
-            </Link>
+          <div className="column is-one-third">
+            <h4 className="spanborder">
+              <span className="title is-4 has-text-weight-bold">
+                Recommended Videos
+              </span>
+            </h4>
+            <VideosRoll videos={videos} />
+            <div className="cta-wrapper has-text-centered">
+              <Link
+                to="/recommended/videos"
+                className="button k-button k-primary raised has-gradient is-bold"
+              >
+                <span className="text">View More Videos</span>
+                <span className="front-gradient"></span>
+              </Link>
+            </div>
           </div>
-        </div>
-        <div className="column is-one-third">
-          <FeaturedSidebar items={featured} />
+          <div className="column is-one-third">
+            <FeaturedSidebar items={featured} />
+          </div>
         </div>
       </div>
     </div>
