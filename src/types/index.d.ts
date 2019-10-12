9fe5add96ff0e0e86da51c49a06bb6541346bd5f
Abhith <dearabhith@gmail.com>
2019-10-12 14:00:06 +0400

    chore: add custom types

---
+++ src/types/index.d.ts
@@ -0,0 +1,33 @@
+export interface IArticle {
+  title: string;
+  slug: string;
+  // authors: IAuthor[];
+  excerpt: string;
+  body: string;
+  id: string;
+  hero: {
+    full: IGatsbyImageFluid;
+    preview: IGatsbyImageFluid;
+    regular: IGatsbyImageFluid;
+    seo: string;
+  };
+  timeToRead: string;
+  date: string;
+  dateModifiedSeoFormat: string;
+  datePublishedSeoFormat: string;
+  tags: string[];
+}
+
+interface IGatsbyImage {
+  src: string;
+  base64?: string;
+  srcWebp?: string;
+  srcSet?: string;
+  srcSetWebp?: string;
+  tracedSVG?: string;
+}
+
+interface IGatsbyImageFluid extends IGatsbyImage {
+  maxHeight: number;
+  maxWidth: number;
+}
