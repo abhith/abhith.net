5d5af3f3e18d0cf49b0cb791e69aa1c232cd0ebc
Abhith <dearabhith@gmail.com>
2019-10-12 13:59:42 +0400

    chore: update linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
-    globals: {
-      __PATH_PREFIX__: true,
-    },
-    extends: `react-app`,
+  globals: {
+    __PATH_PREFIX__: true
+  },
+  extends: `react-app`,
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error"
   }
+};
