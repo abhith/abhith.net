e2c3ee20038b403a9ec0e5905364882c9e22ba24
Abhith <dearabhith@gmail.com>
2019-10-12 19:45:04 +0400

    chore: port article body from js to tsx

--- src/templates/article.template.tsx
+++ src/templates/article.template.tsx
@@ -1,17 +1,59 @@
 import Layout from "@components/Layout";
 import MDXRenderer from "@components/MDX";
 import SEO from "@components/seo/SEO";
+import TopicsBar from "@components/TopicsBar";
+import { OutboundLink } from "gatsby-plugin-google-analytics";
 
-import { graphql } from "gatsby";
+import { graphql, useStaticQuery } from "gatsby";
 import React from "react";
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  LinkedinShareButton,
+  LinkedinIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  WhatsappShareButton,
+  WhatsappIcon,
+  RedditShareButton,
+  RedditIcon,
+  PocketShareButton,
+  PocketIcon
+} from "react-share";
 import { IArticle } from "@types";
+import { FaCoffee } from "react-icons/fa";
 
 import ArticleHero from "../sections/article/Article.Hero";
 
-export default ({ pageContext }) => {
+const siteQuery = graphql`
+  {
+    site {
+      siteMetadata {
+        siteUrl
+        social {
+          twitter
+        }
+        author {
+          name
+          minibio
+          url
+          image
+        }
+      }
+    }
+  }
+`;
+
+export default ({ pageContext, location }) => {
   console.log(pageContext);
 
   const { article }: { article: IArticle } = pageContext;
+  const results = useStaticQuery(siteQuery);
+  console.log(results);
+  const githubURL = `https://github.com/Abhith/abhith.net/blob/master/src/pages${article.slug.substring(
+    0,
+    article.slug.length - 1
+  )}.md`;
   // const commentId =
   //   post.frontmatter.commentId === null
   //     ? post.fields.slug
@@ -29,70 +71,134 @@ export default ({ pageContext }) => {
           dateModified={article.dateModifiedSeoFormat}
           datePublished={article.datePublishedSeoFormat}
         />
-        <ArticleHero article={article}></ArticleHero>
-        <MDXRenderer content={article.body}>
-          <h2>TEST</h2>
-        </MDXRenderer>
+        <ArticleHero article={article} />
+        <section className="section">
+          <div className="container is-fluid">
+            <div className="columns is-centered">
+              <div className="column is-2 pr-4 mb-4">
+                {/* TODO: separate as a section */}
+                <div className="sticky has-text-centered">
+                  <div className="text-muted">Share this</div>
+                  <div className="buttons is-centered">
+                    <FacebookShareButton
+                      url={location.href}
+                      quote={article.title}
+                      className="button is-medium is-white"
+                    >
+                      <FacebookIcon size={44} round />
+                    </FacebookShareButton>
+
+                    <TwitterShareButton
+                      url={location.href}
+                      className="button is-medium is-white"
+                      title={article.title}
+                      via={results.site.siteMetadata.social.twitter
+                        .split("@")
+                        .join("")}
+                      hashtags={article.tags}
+                    >
+                      <TwitterIcon size={44} round></TwitterIcon>
+                    </TwitterShareButton>
+                    <LinkedinShareButton
+                      url={location.href}
+                      className="button is-medium is-white"
+                      title={article.title}
+                    >
+                      <LinkedinIcon size={44} round></LinkedinIcon>
+                    </LinkedinShareButton>
+                    <RedditShareButton
+                      url={location.href}
+                      className="button is-medium is-white"
+                      title={article.title}
+                    >
+                      <RedditIcon size={44} round></RedditIcon>
+                    </RedditShareButton>
+                    <PocketShareButton
+                      url={location.href}
+                      className="button is-medium is-white"
+                      title={article.title}
+                    >
+                      <PocketIcon size={44} round></PocketIcon>
+                    </PocketShareButton>
+                    <WhatsappShareButton
+                      url={location.href}
+                      className="button is-medium is-white"
+                      title={article.title}
+                    >
+                      <WhatsappIcon size={44} round></WhatsappIcon>
+                    </WhatsappShareButton>
+                  </div>
+                </div>
+              </div>
+              <div className="column is-8">
+                <MDXRenderer content={article.body}>
+                  <TopicsBar topics={article.tags} />
+                  <div id="typo" className="bd-typo">
+                    <p className="has-text-grey">
+                      This page is{" "}
+                      <strong className="has-text-grey">open source</strong>.
+                      Noticed a typo? Or something unclear?
+                      <br />
+                      <OutboundLink
+                        href={githubURL}
+                        target="_blank"
+                        className="has-text-grey"
+                      >
+                        Improve this page on GitHub
+                      </OutboundLink>
+                    </p>
+                  </div>
+                  <div className="container mt-5">
+                    <div className="media">
+                      <figure className="media-left">
+                        <p className="image is-96x96">
+                          <img
+                            className="is-rounded"
+                            src={`https://www.abhith.net/img/abhith-avatar.jpg`}
+                            alt={`Abhith Rajan`}
+                          />
+                        </p>
+                      </figure>
+                      <div className="media-content">
+                        <div className="content">
+                          <p>
+                            <strong className="title is-4">
+                              Written by {`Abhith Rajan`}
+                            </strong>{" "}
+                            <span>
+                              <OutboundLink
+                                target="_blank"
+                                className="button is-success is-outlined is-small is-rounded ml-1"
+                                href={`https://twitter.com/abhithrajan`}
+                              >
+                                Follow
+                              </OutboundLink>
+                            </span>
+                            <span>
+                              <OutboundLink
+                                className="button is-info is-outlined is-small is-rounded ml-1"
+                                href="https://ko-fi.com/abhith"
+                                target="_blank"
+                              >
+                                Buy me a coffee{" "}
+                                <FaCoffee className="text-danger" />
+                              </OutboundLink>
+                            </span>
+                            <br />
+                            {`
+        Abhith Rajan is an aspiring software engineer with more than 6 years of experience and proven successful track record of delivering technology-based products and services.
+      `}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </MDXRenderer>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </Layout>
   );
 };
-
-// export const pageQuery = graphql`
-//   query BlogPostQuery($id: String) {
-//     mdx(id: { eq: $id }) {
-//       id
-//       body
-//       fields {
-//         slug
-//         readingTime {
-//           text
-//         }
-//       }
-//       frontmatter {
-//         date
-//         dateString: date(formatString: "MMMM DD, YYYY")
-//         datePublishedSeoFormat: date(formatString: "YYYY-MM-DD")
-//         title
-//         description
-//         tags
-//         lastModificationTime
-//         lastModificationTimeString: lastModificationTime(
-//           formatString: "MMMM DD, YYYY"
-//         )
-//         dateModifiedSeoFormat: lastModificationTime(formatString: "YYYY-MM-DD")
-//         image {
-//           childImageSharp {
-//             fluid(maxWidth: 2048, quality: 100) {
-//               ...GatsbyImageSharpFluid_withWebp
-//             }
-//           }
-//         }
-//         commentId
-//       }
-//     }
-//   }
-// `;
-
-// import React from "react";
-// import { graphql } from "gatsby";
-// import { MDXRenderer } from "gatsby-plugin-mdx";
-// export default function PageTemplate({ data: { mdx } }) {
-//   return (
-//     <div>
-//       <h1>{mdx.frontmatter.title}</h1>
-//       <MDXRenderer>{mdx.body}</MDXRenderer>
-//     </div>
-//   );
-// }
-// export const pageQuery = graphql`
-//   query BlogPostQuery($id: String) {
-//     mdx(id: { eq: $id }) {
-//       id
-//       body
-//       frontmatter {
-//         title
-//       }
-//     }
-//   }
-// `;
