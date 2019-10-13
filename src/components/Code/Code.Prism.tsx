7b916e3e689b652e58aa471b98917ebdf90c7bd0
Abhith <dearabhith@gmail.com>
2019-10-13 22:58:14 +0400

    feat: add code highlight with theme

--- src/components/Code/Code.Prism.tsx
+++ src/components/Code/Code.Prism.tsx
@@ -1,87 +1,48 @@
 import React, { useState } from "react";
 import Highlight, { defaultProps } from "prism-react-renderer";
 import styled from "@emotion/styled";
-// import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
-import theme from "prism-react-renderer/themes/oceanicNext";
+import theme from "prism-react-renderer/themes/nightOwl";
 
-// import Icons from "@icons";
 import mediaqueries from "@styles/media";
 import { copyToClipboard } from "@utils";
 import { FaCopy, FaClipboard } from "react-icons/fa";
 
-const RE = /{([\d,-]+)}/;
+const Pre = styled.pre`
+  position: relative;
+`;
 
-function calculateLinesToHighlight(meta) {
-  if (RE.test(meta)) {
-    const lineNumbers = RE.exec(meta)[1]
-      .split(",")
-      .map(v => v.split("-").map(y => parseInt(y, 10)));
-
-    return index => {
-      const lineNumber = index + 1;
-      const inRange = lineNumbers.some(([start, end]) =>
-        end ? lineNumber >= start && lineNumber <= end : lineNumber === start
-      );
-      return inRange;
-    };
-  } else {
-    return () => false;
-  }
-}
+const LineNo = styled.span`
+  display: inline-block;
+  width: 2em;
+  user-select: none;
+  opacity: 0.3;
+`;
 
 function CodePrism({ codeString, language, metastring, ...props }) {
-  const shouldHighlightLine = calculateLinesToHighlight(metastring);
-
-  //   if (props["live"]) {
-  //     return (
-  //       <Container>
-  //         <LiveProvider code={codeString} noInline={true} theme={theme}>
-  //           <LiveEditor style={{ marginBottom: "3px", borderRadius: "2px" }} />
-  //           <LivePreview style={{ fontSize: "18px", borderRadius: "2px" }} />
-  //           <LiveError style={{ color: "tomato" }} />
-  //         </LiveProvider>
-  //       </Container>
-  //     );
-  //   } else {
   return (
-    <Highlight {...defaultProps} code={codeString} language={language}>
-      {({ className, tokens, getLineProps, getTokenProps }) => {
-        return (
-          <div style={{ overflow: "auto" }}>
-            <pre className={className} style={{ position: "relative" }}>
-              <Copy toCopy={codeString} />
-              {tokens.map((line, index) => {
-                const { className } = getLineProps({
-                  line,
-                  key: index,
-                  className: shouldHighlightLine(index) ? "highlight-line" : ""
-                });
-
-                return (
-                  <div key={index} className={className}>
-                    <span className="number-line">{index + 1}</span>
-                    {line.map((token, key) => {
-                      const { className, children } = getTokenProps({
-                        token,
-                        key
-                      });
-
-                      return (
-                        <span key={key} className={className}>
-                          {children}
-                        </span>
-                      );
-                    })}
-                  </div>
-                );
-              })}
-            </pre>
-          </div>
-        );
-      }}
+    <Highlight
+      {...defaultProps}
+      theme={theme}
+      code={codeString}
+      language={language}
+    >
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <div style={{ overflow: "auto" }}>
+          <Pre className={className} style={style}>
+            <Copy toCopy={codeString} />
+            {tokens.map((line, i) => (
+              <div {...getLineProps({ line, key: i })}>
+                <LineNo>{i + 1}</LineNo>
+                {line.map((token, key) => (
+                  <span {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </Pre>
+        </div>
+      )}
     </Highlight>
   );
-  //   }
 }
 
 export default CodePrism;
@@ -90,7 +51,9 @@ function Copy({ toCopy }: { toCopy: string }) {
   const [hasCopied, setHasCopied] = useState<boolean>(false);
 
   function copyToClipboardOnClick() {
-    if (hasCopied) return;
+    if (hasCopied) {
+      return;
+    }
 
     copyToClipboard(toCopy);
     setHasCopied(true);
@@ -104,13 +67,11 @@ function Copy({ toCopy }: { toCopy: string }) {
     <CopyButton onClick={copyToClipboardOnClick} data-a11y="false">
       {hasCopied ? (
         <>
-          {/* Copied <Icons.Copied fill="#6f7177" />  */}
           Copied <FaClipboard></FaClipboard>
         </>
       ) : (
         <>
           Copy <FaCopy />
-          {/* Copy <Icons.Copy fill="#6f7177" /> */}
         </>
       )}
     </CopyButton>
@@ -125,6 +86,14 @@ const CopyButton = styled.button`
   border-radius: 5px;
   color: #6f7177;
   transition: background 0.3s ease;
+
+  -webkit-appearance: none;
+  border-width: initial;
+  border-style: none;
+  border-color: initial;
+  border-image: initial;
+  outline: none;
+  background: transparent;
 
   &:hover {
     background: rgba(255, 255, 255, 0.07);
@@ -145,49 +114,4 @@ const CopyButton = styled.button`
   ${mediaqueries.tablet`
     display: none;
   `}
-`;
-
-const monospace = `"Operator Mono", Consolas, Menlo, Monaco, source-code-pro, Courier New, monospace`;
-
-const Container = styled.div`
-  overflow: scroll;
-  width: 100%;
-  max-width: 750px;
-  margin: 0 auto;
-  font-size: 13px;
-  margin: 15px auto 50px;
-  border-radius: 5px;
-  font-family: ${monospace} !important;
-
-  textarea,
-  pre {
-    padding: 32px !important;
-    font-family: ${monospace} !important;
-  }
-
-  ${mediaqueries.desktop`
-      left: -26px;
-    `};
-
-  ${mediaqueries.tablet`
-    max-width: 526px;
-    left: 0;
-
-    textarea,
-    pre {
-      padding: 20px !important;
-    }
-  `};
-
-  ${mediaqueries.phablet`
-    border-radius: 0;
-    margin: 0 auto 25px;
-    overflow: initial;
-    width: unset;
-    max-width: unset;
-    float: left;
-    min-width: 100%;
-    overflow: initial;
-    position: relative;
-  `};
 `;
