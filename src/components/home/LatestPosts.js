2cb5f1bcb61468256955ad42115258bdbff0f028
Abhith <dearabhith@gmail.com>
2019-10-14 21:05:33 +0400

    refactor: update home latest posts to mdx

--- src/components/home/LatestPosts.js
+++ src/components/home/LatestPosts.js
@@ -2,15 +2,22 @@ import React from "react";
 import { graphql, StaticQuery } from "gatsby";
 import BlogRollItem from "../BlogRollItem";
 import BlogCard from "../blog/BlogCard";
+const normalize = require("../../../gatsby/data/data.normalize");
 
 function LatestPosts() {
   return (
     <StaticQuery
       query={query}
       render={data => {
-        const latestPost = data.latestPost.edges[0].node;
-        const lastUpdatedPost = data.lastUpdatedPost.edges[0].node;
-        const recentPosts = data.recentPosts.edges;
+        const latestPost = data.latestPost.edges.map(
+          normalize.local.articles
+        )[0];
+        const lastUpdatedPost = data.lastUpdatedPost.edges.map(
+          normalize.local.articles
+        )[0];
+        const recentPosts = data.recentPosts.edges.map(
+          normalize.local.articles
+        );
 
         return (
           <section className="section">
@@ -18,11 +25,14 @@ function LatestPosts() {
               <div className="columns">
                 <BlogCard post={latestPost}></BlogCard>
                 <div className="column">
-                  {recentPosts.map(({ node }) => {
-                    return <BlogRollItem post={node} key={node.fields.slug} />;
+                  {recentPosts.map(node => {
+                    return <BlogRollItem post={node} key={node.slug} />;
                   })}
                 </div>
-                <BlogCard post={lastUpdatedPost} tag={`Recently Updated`}></BlogCard>
+                <BlogCard
+                  post={lastUpdatedPost}
+                  tag={`Recently Updated`}
+                ></BlogCard>
               </div>
             </div>
           </section>
@@ -36,13 +46,15 @@ export default LatestPosts;
 
 export const query = graphql`
   query {
-    lastUpdatedPost: allMarkdownRemark(
+    lastUpdatedPost: allMdx(
       sort: { fields: [frontmatter___lastModificationTime], order: DESC }
-      filter: { frontmatter: { templateKey: { eq: "blog-post" }, lastModificationTime: { ne: null } }}
+      filter: { frontmatter: { lastModificationTime: { ne: null } } }
       limit: 1
     ) {
       edges {
         node {
+          id
+          body
           fields {
             slug
             readingTime {
@@ -50,28 +62,40 @@ export const query = graphql`
             }
           }
           frontmatter {
-            date(formatString: "MMM DD, YYYY")
+            date
+            dateString: date(formatString: "MMMM DD, YYYY")
+            datePublishedSeoFormat: date(formatString: "YYYY-MM-DD")
             title
-            tags
             description
+            tags
+            lastModificationTime
+            lastModificationTimeString: lastModificationTime(
+              formatString: "MMMM DD, YYYY"
+            )
+            dateModifiedSeoFormat: lastModificationTime(
+              formatString: "YYYY-MM-DD"
+            )
             image {
-              childImageSharp {
-                fluid(maxHeight: 200, quality: 100) {
+              full: childImageSharp {
+                fluid(maxWidth: 2048, quality: 100) {
                   ...GatsbyImageSharpFluid_withWebp
                 }
               }
             }
+            commentId
           }
         }
       }
     }
-    latestPost: allMarkdownRemark(
+    latestPost: allMdx(
       sort: { fields: [frontmatter___date], order: DESC }
       filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
       limit: 1
     ) {
       edges {
         node {
+          id
+          body
           fields {
             slug
             readingTime {
@@ -79,22 +103,32 @@ export const query = graphql`
             }
           }
           frontmatter {
-            date(formatString: "MMM DD, YYYY")
+            date
+            dateString: date(formatString: "MMMM DD, YYYY")
+            datePublishedSeoFormat: date(formatString: "YYYY-MM-DD")
             title
-            tags
             description
+            tags
+            lastModificationTime
+            lastModificationTimeString: lastModificationTime(
+              formatString: "MMMM DD, YYYY"
+            )
+            dateModifiedSeoFormat: lastModificationTime(
+              formatString: "YYYY-MM-DD"
+            )
             image {
-              childImageSharp {
-                fluid(maxHeight: 200, quality: 100) {
+              full: childImageSharp {
+                fluid(maxWidth: 2048, quality: 100) {
                   ...GatsbyImageSharpFluid_withWebp
                 }
               }
             }
+            commentId
           }
         }
       }
     }
-    recentPosts: allMarkdownRemark(
+    recentPosts: allMdx(
       sort: { fields: [frontmatter___date], order: DESC }
       filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
       skip: 1
@@ -102,7 +136,8 @@ export const query = graphql`
     ) {
       edges {
         node {
-          excerpt
+          id
+          body
           fields {
             slug
             readingTime {
@@ -110,16 +145,27 @@ export const query = graphql`
             }
           }
           frontmatter {
-            date(formatString: "MMM DD, YYYY")
+            date
+            dateString: date(formatString: "MMMM DD, YYYY")
+            datePublishedSeoFormat: date(formatString: "YYYY-MM-DD")
             title
+            description
             tags
+            lastModificationTime
+            lastModificationTimeString: lastModificationTime(
+              formatString: "MMMM DD, YYYY"
+            )
+            dateModifiedSeoFormat: lastModificationTime(
+              formatString: "YYYY-MM-DD"
+            )
             image {
-              childImageSharp {
-                fluid(maxWidth: 375, quality: 100) {
+              full: childImageSharp {
+                fluid(maxWidth: 2048, quality: 100) {
                   ...GatsbyImageSharpFluid_withWebp
                 }
               }
             }
+            commentId
           }
         }
       }
