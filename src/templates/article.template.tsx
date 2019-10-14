f65890d7c192067f4d558f5accfc01e629a47639
Abhith <dearabhith@gmail.com>
2019-10-14 19:39:41 +0400

    refactor: add separate section for article share

--- src/templates/article.template.tsx
+++ src/templates/article.template.tsx
@@ -6,24 +6,12 @@ import { OutboundLink } from "gatsby-plugin-google-analytics";
 
 import { graphql, useStaticQuery } from "gatsby";
 import React from "react";
-import {
-  FacebookShareButton,
-  FacebookIcon,
-  LinkedinShareButton,
-  LinkedinIcon,
-  TwitterShareButton,
-  TwitterIcon,
-  WhatsappShareButton,
-  WhatsappIcon,
-  RedditShareButton,
-  RedditIcon,
-  PocketShareButton,
-  PocketIcon
-} from "react-share";
+
 import { IArticle } from "@types";
 import { FaCoffee } from "react-icons/fa";
 
 import ArticleHero from "../sections/article/Article.Hero";
+import ArticleShare from "../sections/article/Article.Share";
 
 const siteQuery = graphql`
   {
@@ -76,59 +64,7 @@ export default ({ pageContext, location }) => {
           <div className="container is-fluid">
             <div className="columns is-centered">
               <div className="column is-2 pr-4 mb-4">
-                {/* TODO: separate as a section */}
-                <div className="sticky has-text-centered">
-                  <div className="text-muted">Share this</div>
-                  <div className="buttons is-centered">
-                    <FacebookShareButton
-                      url={location.href}
-                      quote={article.title}
-                      className="button is-medium is-white"
-                    >
-                      <FacebookIcon size={44} round />
-                    </FacebookShareButton>
-
-                    <TwitterShareButton
-                      url={location.href}
-                      className="button is-medium is-white"
-                      title={article.title}
-                      via={results.site.siteMetadata.social.twitter
-                        .split("@")
-                        .join("")}
-                      hashtags={article.tags}
-                    >
-                      <TwitterIcon size={44} round></TwitterIcon>
-                    </TwitterShareButton>
-                    <LinkedinShareButton
-                      url={location.href}
-                      className="button is-medium is-white"
-                      title={article.title}
-                    >
-                      <LinkedinIcon size={44} round></LinkedinIcon>
-                    </LinkedinShareButton>
-                    <RedditShareButton
-                      url={location.href}
-                      className="button is-medium is-white"
-                      title={article.title}
-                    >
-                      <RedditIcon size={44} round></RedditIcon>
-                    </RedditShareButton>
-                    <PocketShareButton
-                      url={location.href}
-                      className="button is-medium is-white"
-                      title={article.title}
-                    >
-                      <PocketIcon size={44} round></PocketIcon>
-                    </PocketShareButton>
-                    <WhatsappShareButton
-                      url={location.href}
-                      className="button is-medium is-white"
-                      title={article.title}
-                    >
-                      <WhatsappIcon size={44} round></WhatsappIcon>
-                    </WhatsappShareButton>
-                  </div>
-                </div>
+                <ArticleShare article={article} />
               </div>
               <div className="column is-8">
                 <MDXRenderer content={article.body}>
@@ -185,6 +121,7 @@ export default ({ pageContext, location }) => {
                               </OutboundLink>
                             </span>
                             <br />
+                            {/* TODO: Make dynamic */}
                             {`
         Abhith Rajan is an aspiring software engineer with more than 6 years of experience and proven successful track record of delivering technology-based products and services.
       `}
