776ee9e151e725108a3b645a80d063465d16c649
Abhith <dearabhith@gmail.com>
2019-10-26 15:24:38 +0400

    chore(ui): mobile improvements

--- src/templates/article.template.tsx
+++ src/templates/article.template.tsx
@@ -53,77 +53,75 @@ export default ({ pageContext, location }) => {
         />
         <ArticleHero article={article} />
         <section className="section">
-          <div className="container is-fluid">
-            <div className="columns is-centered">
-              <div className="column is-2 pr-4 mb-4">
-                <ArticleShare article={article} location={location} />
-              </div>
-              <div className="column is-8">
-                <MDXRenderer content={article.body}>
-                  <TopicsBar topics={article.tags} />
-                  <div id="typo" className="bd-typo">
-                    <p className="has-text-grey">
-                      This page is{" "}
-                      <strong className="has-text-grey">open source</strong>.
-                      Noticed a typo? Or something unclear?
-                      <br />
-                      <OutboundLink
-                        href={githubURL}
-                        target="_blank"
-                        className="has-text-grey"
-                      >
-                        Improve this page on GitHub
-                      </OutboundLink>
-                    </p>
-                  </div>
-                  <div className="container mt-5">
-                    <div className="media">
-                      <figure className="media-left">
-                        <p className="image is-96x96">
-                          <img
-                            className="is-rounded"
-                            src={`https://www.abhith.net/img/abhith-avatar.jpg`}
-                            alt={`Abhith Rajan`}
-                          />
-                        </p>
-                      </figure>
-                      <div className="media-content">
-                        <div className="content">
-                          <p>
-                            <strong className="title is-4">
-                              Written by {`Abhith Rajan`}
-                            </strong>{" "}
-                            <span>
-                              <OutboundLink
-                                target="_blank"
-                                className="button is-success is-outlined is-small is-rounded ml-1"
-                                href={`https://twitter.com/abhithrajan`}
-                              >
-                                Follow
-                              </OutboundLink>
-                            </span>
-                            <span>
-                              <OutboundLink
-                                className="button is-info is-outlined is-small is-rounded ml-1"
-                                href="https://ko-fi.com/abhith"
-                                target="_blank"
-                              >
-                                Buy me a coffee{" "}
-                                <FaCoffee className="text-danger" />
-                              </OutboundLink>
-                            </span>
-                            <br />
-                            {/* TODO: Make dynamic */}
-                            {`
+          <div className="columns is-centered">
+            <div className="column is-2 pr-4 mb-4">
+              <ArticleShare article={article} location={location} />
+            </div>
+            <div className="column is-8">
+              <MDXRenderer content={article.body}>
+                <TopicsBar topics={article.tags} />
+                <div id="typo" className="bd-typo">
+                  <p className="has-text-grey">
+                    This page is{" "}
+                    <strong className="has-text-grey">open source</strong>.
+                    Noticed a typo? Or something unclear?
+                    <br />
+                    <OutboundLink
+                      href={githubURL}
+                      target="_blank"
+                      className="has-text-grey"
+                    >
+                      Improve this page on GitHub
+                    </OutboundLink>
+                  </p>
+                </div>
+                <div className="container mt-5">
+                  <div className="media">
+                    <figure className="media-left">
+                      <p className="image is-96x96">
+                        <img
+                          className="is-rounded"
+                          src={`https://www.abhith.net/img/abhith-avatar.jpg`}
+                          alt={`Abhith Rajan`}
+                        />
+                      </p>
+                    </figure>
+                    <div className="media-content">
+                      <div className="content">
+                        <p>
+                          <strong className="title is-4">
+                            Written by {`Abhith Rajan`}
+                          </strong>{" "}
+                          <span>
+                            <OutboundLink
+                              target="_blank"
+                              className="button is-success is-outlined is-small is-rounded ml-1"
+                              href={`https://twitter.com/abhithrajan`}
+                            >
+                              Follow
+                            </OutboundLink>
+                          </span>
+                          <span>
+                            <OutboundLink
+                              className="button is-info is-outlined is-small is-rounded ml-1"
+                              href="https://ko-fi.com/abhith"
+                              target="_blank"
+                            >
+                              Buy me a coffee{" "}
+                              <FaCoffee className="text-danger" />
+                            </OutboundLink>
+                          </span>
+                          <br />
+                          {/* TODO: Make dynamic */}
+                          {`
         Abhith Rajan is an aspiring software engineer with more than 6 years of experience and proven successful track record of delivering technology-based products and services.
       `}
-                          </p>
-                        </div>
+                        </p>
                       </div>
                     </div>
                   </div>
-                </MDXRenderer>
-              </div>
+                </div>
+              </MDXRenderer>
             </div>
           </div>
         </section>
