d3af4484ebc0bebfa7fbe60722dda032b5580bc5
Abhith <dearabhith@gmail.com>
2019-12-26 22:30:59 +0400

    chore(deps): #422

--- gatsby-config.js
+++ gatsby-config.js
@@ -197,6 +197,13 @@ module.exports = {
       }
     },
     {
+      resolve: `gatsby-plugin-google-adsense`,
+      options: {
+        googleAdClientId: "ca-pub-5964640183793435",
+        head: false // Optional
+      }
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Abhith Rajan`,
