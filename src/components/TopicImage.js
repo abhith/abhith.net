c82ba0d5921f0c0335bc1fddc2769efaa39357a1
Abhith <dearabhith@gmail.com>
2019-12-27 23:29:09 +0400

    refactor: remove all image query

--- src/components/TopicImage.js
+++ src/components/TopicImage.js
@@ -1,21 +1,10 @@
 import React from "react";
-import Img from "gatsby-image";
 import { graphql, StaticQuery } from "gatsby";
 
 const TopicImage = ({ slug }) => (
   <StaticQuery
     query={graphql`
       query {
-        allImageSharp {
-          edges {
-            node {
-              fluid(maxWidth: 500) {
-                ...GatsbyImageSharpFluid_withWebp
-                originalName
-              }
-            }
-          }
-        }
         allTopicsJson {
           edges {
             node {
@@ -35,36 +24,16 @@ const TopicImage = ({ slug }) => (
       const noImageName = "no_image_available.svg";
 
       let topicImageName = tagNode ? tagNode.node.image : noImageName;
-      // empty string check
+
       if (topicImageName === "" || topicImageName === null) {
         topicImageName = noImageName;
       }
       const imageAlt = tagNode ? tagNode.node.title : "No Image";
 
-      // console.log(`${slug} - ${topicImageName}`);
-
-      var ext = topicImageName.substr(topicImageName.lastIndexOf(".") + 1);
-
-      if (ext === "svg") {
-        const pathToImage = `/img/topics/${topicImageName}`;
-        return (
-          <figure className="image">
-            <img src={pathToImage} alt={imageAlt}></img>
-          </figure>
-        );
-      }
-
-      const image = data.allImageSharp.edges.find(
-        edge => edge.node.fluid.originalName === topicImageName
-      );
-
-      if (!image) {
-        return null;
-      }
-
+      const pathToImage = `/img/topics/${topicImageName}`;
       return (
         <figure className="image">
-          <Img fluid={image.node.fluid} alt={imageAlt} />
+          <img src={pathToImage} alt={imageAlt}></img>
         </figure>
       );
     }}
