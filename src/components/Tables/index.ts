233ec4a181d6c31fa8cf9bc3d1a37d0d991e51d1
Abhith <dearabhith@gmail.com>
2020-01-25 11:51:45 +0400

    style: sort imports

--- src/components/Tables/index.ts
+++ src/components/Tables/index.ts
@@ -1,7 +1,7 @@
+import Table from "./Table";
 import Cell from "./Table.Cell";
 import Head from "./Table.Head";
 import HeadCell from "./Table.Head.Cell";
-import Table from "./Table";
 
 export default {
   Cell,
