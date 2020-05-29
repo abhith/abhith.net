b8a125f823ec11ec1b2d4ca1e5b98613be4d18eb
Abhith <dearabhith@gmail.com>
2020-05-29 22:19:22 +0400

    ui: change page section size

--- src/templates/page-template.js
+++ src/templates/page-template.js
@@ -2,22 +2,23 @@ import Layout from "@components/layout";
 import SEO from "@components/seo/seo";
 import React from "react";
 
-const PageTemplate = props => {
+const PageTemplate = (props) => {
   const {
-    frontmatter: { title, description }
+    frontmatter: { title, description },
   } = props.pageContext;
 
   return (
     <Layout>
-      <div className="container">
-        <SEO title={title} description={description} slug="/donate" />
-        <section className="section">
+      {/* TODO(abhith): remove hard coded slug */}
+      <SEO title={title} description={description} slug="/donate" />
+      <section className="section is-medium">
+        <div className="container">
           <h1 className="title spanborder has-text-weight-bold">
             <span> {title}</span>
           </h1>
           <div className="page-content content">{props.children}</div>
-        </section>
-      </div>
+        </div>
+      </section>
     </Layout>
   );
 };
