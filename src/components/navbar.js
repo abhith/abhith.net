cfe90eac8b68d1694077b5659b23e1a08cc0fbc7
Abhith <dearabhith@gmail.com>
2020-06-06 20:21:28 +0400

    ui: update navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -49,18 +49,107 @@ const Navbar = class extends React.Component {
   render() {
     return (
       <nav className="navbar is-spaced" id="ar-navbar">
-        <div className="container">
-          <div className="navbar-brand">
+        <div className="navbar-brand">
+          <Link to="/" className="navbar-item">
+            <figure className="image is-vertical-center">
+              <Img
+                fluid={this.props.logo.childImageSharp.fluid}
+                className="nav-logo"
+              />
+            </figure>
+          </Link>
+          <OutboundLink
+            className="navbar-item is-hidden-desktop"
+            href="https://github.com/Abhith/abhith.net"
+            target="_blank"
+          >
+            <span className="icon">
+              <GoMarkGithub size={24} />
+            </span>
+          </OutboundLink>
+          <OutboundLink
+            className="navbar-item is-hidden-desktop"
+            href="https://twitter.com/AbhithRajan"
+            target="_blank"
+          >
+            <span className="icon" style={{ color: "#55acee" }}>
+              <FaTwitter size={24} />
+            </span>
+          </OutboundLink>
+          <div
+            role="button"
+            className={`navbar-burger burger ${this.state.hamburgerMenuClass}`}
+            onClick={() => this.toggleHamburger()}
+            onKeyDown={() => this.toggleHamburger()}
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="ar-navMenu"
+            tabIndex={0}
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </div>
+        </div>
+        <div
+          id="ar-navMenu"
+          className={`navbar-menu ${this.state.navBarActiveClass}`}
+        >
+          <div className="navbar-start">
             <Link to="/" className="navbar-item">
-              <figure className="image is-vertical-center">
-                <Img
-                  fluid={this.props.logo.childImageSharp.fluid}
-                  className="nav-logo"
-                />
-              </figure>
+              <span className="icon">
+                <FcHome />
+              </span>
+              <span>Home</span>
             </Link>
+            <Link to="/about/" className="navbar-item">
+              <span className="icon">
+                <FcPortraitMode />
+              </span>
+              <span>About</span>
+            </Link>
+            <Link to="/blog/" className="navbar-item">
+              <span className="icon">
+                <FcReading></FcReading>
+              </span>
+              <span>Blog</span>
+            </Link>
+            <Link to="/recommended/" className="navbar-item">
+              <span className="icon">
+                <FcLike />
+              </span>
+              <span>Recommended</span>
+            </Link>
+            <Link to="/topics/" className="navbar-item">
+              <span className="icon">
+                <FcTimeline />
+              </span>
+              <span>Topics</span>
+            </Link>
+            <Link to="/contact/" className="navbar-item">
+              <span className="icon">
+                <FcContacts />
+              </span>
+              <span>Contact</span>
+            </Link>
+          </div>
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="field is-grouped">
+                <OutboundLink
+                  className="is-bold button k-button k-secondary raised has-gradient"
+                  href="https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6"
+                  target="_blank"
+                >
+                  <FaShieldAlt color="black" size={24} />
+                  <span className="ml-1">
+                    Corona is UNBIASED | Break The Chain
+                  </span>
+                </OutboundLink>
+              </div>
+            </div>
             <OutboundLink
-              className="navbar-item is-hidden-desktop"
+              className="navbar-item is-hidden-desktop-only"
               href="https://github.com/Abhith/abhith.net"
               target="_blank"
             >
@@ -69,7 +158,7 @@ const Navbar = class extends React.Component {
               </span>
             </OutboundLink>
             <OutboundLink
-              className="navbar-item is-hidden-desktop"
+              className="navbar-item is-hidden-desktop-only"
               href="https://twitter.com/AbhithRajan"
               target="_blank"
             >
@@ -77,108 +166,16 @@ const Navbar = class extends React.Component {
                 <FaTwitter size={24} />
               </span>
             </OutboundLink>
-            <div
-              role="button"
-              className={`navbar-burger burger ${this.state.hamburgerMenuClass}`}
-              onClick={() => this.toggleHamburger()}
-              onKeyDown={() => this.toggleHamburger()}
-              aria-label="menu"
-              aria-expanded="false"
-              data-target="ar-navMenu"
-              tabIndex={0}
-            >
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
-            </div>
-          </div>
-
-          <div
-            id="ar-navMenu"
-            className={`navbar-menu ${this.state.navBarActiveClass}`}
-          >
-            <div className="navbar-start">
-              <Link to="/" className="navbar-item">
-                <span className="icon">
-                  <FcHome />
-                </span>
-                <span>Home</span>
-              </Link>
-              <Link to="/about/" className="navbar-item">
-                <span className="icon">
-                  <FcPortraitMode />
-                </span>
-                <span>About</span>
-              </Link>
-              <Link to="/blog/" className="navbar-item">
-                <span className="icon">
-                  <FcReading></FcReading>
-                </span>
-                <span>Blog</span>
-              </Link>
-              <Link to="/recommended/" className="navbar-item">
-                <span className="icon">
-                  <FcLike />
-                </span>
-                <span> Recommended</span>
-              </Link>
-              <Link to="/topics/" className="navbar-item">
-                <span className="icon">
-                  <FcTimeline />
-                </span>
-                <span>Topics</span>
-              </Link>
-              <Link to="/contact/" className="navbar-item">
-                <span className="icon">
-                  <FcContacts />
-                </span>
-                <span>Contact</span>
-              </Link>
-              <OutboundLink
-                className="is-bold button k-button k-secondary raised has-gradient"
-                href="https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6"
-                target="_blank"
-              >
-                <FaShieldAlt color="black" size={24} />
-                <span className="ml-1">
-                  Corona is UNBIASED | Break The Chain
-                </span>
-              </OutboundLink>
-            </div>
-
-            <div className="navbar-end">
-              <OutboundLink
-                className="navbar-item is-hidden-desktop-only"
-                href="https://github.com/Abhith/abhith.net"
-                target="_blank"
-              >
-                <span className="icon">
-                  <GoMarkGithub size={24} />
-                </span>
-              </OutboundLink>
-              <OutboundLink
-                className="navbar-item is-hidden-desktop-only"
-                href="https://twitter.com/AbhithRajan"
-                target="_blank"
-              >
-                <span className="icon" style={{ color: "#55acee" }}>
-                  <FaTwitter size={24} />
-                </span>
-              </OutboundLink>
-
-              <div className="navbar-item">
-                <div className="field is-grouped">
-                  <p className="control">
-                    <Link
-                      to="/donate/"
-                      className="button k-button k-primary raised has-gradient slanted"
-                    >
-                      <span>
-                        BUY ME A <FaCoffee />
-                      </span>
-                    </Link>
-                  </p>
-                </div>
+            <div className="navbar-item">
+              <div className="field is-grouped">
+                <Link
+                  to="/donate/"
+                  className="button k-button k-primary raised has-gradient slanted"
+                >
+                  <span>
+                    BUY ME A <FaCoffee />
+                  </span>
+                </Link>
               </div>
             </div>
           </div>
