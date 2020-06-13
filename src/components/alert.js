7a67543bd5544d66362ea8a434882e7e44a68d9e
Abhith <dearabhith@gmail.com>
2020-06-13 11:37:02 +0400

    ui: add icon to alert component

--- src/components/alert.js
+++ src/components/alert.js
@@ -1,11 +1,14 @@
-import React, { SFC } from "react";
+import React from "react";
 import styled from "@emotion/styled";
+import { FcHighPriority, FcIdea, FcInfo } from "react-icons/fc";
+import { capitalize } from "lodash";
 
-const kinds = {
+const kindsBackgroundColor = {
   info: "#5352ED",
   positive: "#2ED573",
   negative: "#FF4757",
   warning: "#fffbeb",
+  important: "#fffbeb",
 };
 
 const kindsColor = {
@@ -13,6 +16,13 @@ const kindsColor = {
   positive: "#2ED573",
   negative: "#FF4757",
   warning: "#947600",
+  important: "#947600",
+};
+
+const kindsIcon = {
+  important: FcHighPriority,
+  idea: FcIdea,
+  info: FcInfo,
 };
 
 const AlertStyled = styled("div")`
@@ -20,12 +30,27 @@ const AlertStyled = styled("div")`
   background: white;
   border-radius: 3px;
   color: white;
-  background: ${({ kind = "info" }) => kinds[kind]};
+  background: ${({ kind = "info" }) => kindsBackgroundColor[kind]};
   color: ${({ kind = "info" }) => kindsColor[kind]};
 `;
 
-function Alert({ kind, ...props }) {
-  return <AlertStyled {...props} kind={kind} />;
+function Alert({ kind, children }) {
+  let KindIcon = kindsIcon[kind];
+  return (
+    <AlertStyled kind={kind}>
+      <div className="level">
+        <div className="level-left">
+          <div className="level-item">
+            <span className="icon">
+              <KindIcon />
+            </span>
+            <span className="title is-5">{capitalize(kind)}</span>
+          </div>
+        </div>
+      </div>
+      <p className="mb-0">{children}</p>
+    </AlertStyled>
+  );
 }
 
 export default Alert;
