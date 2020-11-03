151ab667602638ace9a225e5e7878ea14b43706c
Abhith <dearabhith@gmail.com>
2020-11-03 20:48:25 +0400

    chore: integrate clarity

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,7 +3,7 @@ import React from "react";
 export const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
   const pluginOptions = {
     googleAdClientId: `ca-pub-5964640183793435`,
-    head: true
+    head: true,
   };
 
   if (process.env.NODE_ENV !== `production`) {
@@ -29,8 +29,21 @@ export const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
             google_ad_client: "${pluginOptions.googleAdClientId}",
             enable_page_level_ads: true
         });
-        `
+        `,
       }}
-    />
+    />,
+    <script
+      async
+      type="text/javascript"
+      dangerouslySetInnerHTML={{
+        __html: `
+      (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "3y06r37z6k");
+      `,
+      }}
+    />,
   ]);
 };
