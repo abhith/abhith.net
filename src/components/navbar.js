20f976f67857b228236a26f651d4f15ce1cd9fcf
Abhith Rajan <dearabhith@gmail.com>
2022-04-19 21:42:14 +0400

    refactor: navbar to functional component

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "gatsby";
 import { FaCoffee, FaTwitter } from "react-icons/fa";
 import {
@@ -14,17 +14,10 @@ import { GoMarkGithub } from "react-icons/go";
 import { OutboundLink } from "gatsby-plugin-google-gtag";
 import { StaticQuery, graphql } from "gatsby";
 import { GatsbyImage } from "gatsby-plugin-image";
-import NavbarItem from "@components/navbar-item";
-const Navbar = class extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      active: false,
-      navBarActiveClass: "",
-      hamburgerMenuClass: "",
-    };
-  }
 
+import NavbarItem from "@components/navbar-item";
+
+function Navbar({ logo }) {
   toggleHamburger = () => {
     // toggle the active boolean in the state
     this.setState(
@@ -43,24 +36,99 @@ const Navbar = class extends React.Component {
               navBarActiveClass: "",
               hamburgerMenuClass: "",
             });
-      }
-    );
-  };
+  const [navbarOpen, setNavbarOpen] = useState(false);
 
-  render() {
-    return (
-      <nav className="navbar is-spaced" id="ar-navbar">
-        <div className="navbar-brand">
-          <Link to="/" className="navbar-item">
-            <figure className="image is-vertical-center">
-              <GatsbyImage
-                image={this.props.logo.childImageSharp.gatsbyImageData}
-                alt="logo"
-                className="nav-logo" />
-            </figure>
-          </Link>
+  function toggleHamburger() {
+    setNavbarOpen((prev) => !prev);
+  }
+
+  return (
+    <nav className="navbar is-spaced" id="ar-navbar">
+      <div className="navbar-brand">
+        <Link to="/" className="navbar-item">
+          <figure className="image is-vertical-center">
+            <GatsbyImage
+              image={logo.childImageSharp.gatsbyImageData}
+              alt="logo"
+              className="nav-logo"
+            />
+          </figure>
+        </Link>
+        <OutboundLink
+          className="navbar-item is-hidden-desktop"
+          href="https://github.com/Abhith/abhith.net"
+          target="_blank"
+        >
+          <span className="icon">
+            <GoMarkGithub size={24} />
+          </span>
+        </OutboundLink>
+        <OutboundLink
+          className="navbar-item is-hidden-desktop"
+          href="https://twitter.com/AbhithRajan"
+          target="_blank"
+        >
+          <span className="icon" style={{ color: "#55acee" }}>
+            <FaTwitter size={24} />
+          </span>
+        </OutboundLink>
+        <div
+          role="button"
+          className={`navbar-burger burger ${navbarOpen ? " is-active" : ""}`}
+          onClick={() => toggleHamburger()}
+          onKeyDown={() => toggleHamburger()}
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="ar-navMenu"
+          tabIndex={0}
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </div>
+      </div>
+      <div
+        id="ar-navMenu"
+        className={`navbar-menu ${navbarOpen ? " is-active" : ""}`}
+      >
+        <div className="navbar-start">
+          <NavbarItem to="/" title="Home">
+            <FcHome />
+          </NavbarItem>
+          <NavbarItem to="/about/" title="About">
+            <FcPortraitMode />
+          </NavbarItem>
+          <NavbarItem to="/blog/" title="Blog">
+            <FcReading />
+          </NavbarItem>
+          <NavbarItem to="/snippets/" title="Snippets">
+            <FcPuzzle />
+          </NavbarItem>
+          <NavbarItem to="/recommended/" title="Recommended">
+            <FcLike />
+          </NavbarItem>
+          <NavbarItem to="/topics/" title="Topics">
+            <FcTimeline />
+          </NavbarItem>
+          <NavbarItem to="/contact/" title="Contact">
+            <FcContacts />
+          </NavbarItem>
+        </div>
+        <div className="navbar-end">
+          <div className="navbar-item is-hidden-desktop-only is-hidden-widescreen-only">
+            <div className="field is-grouped">
+              <iframe
+                src="https://ghbtns.com/github-btn.html?user=Abhith&repo=abhith.net&type=star&count=true&size=large"
+                frameBorder="0"
+                scrolling="0"
+                width="170"
+                height="30"
+                title="Star Abhith/abhith.net on GitHub"
+              ></iframe>
+            </div>
+          </div>
           <OutboundLink
-            className="navbar-item is-hidden-desktop"
+            className="navbar-item is-hidden-desktop-only"
             href="https://github.com/Abhith/abhith.net"
             target="_blank"
           >
@@ -69,7 +137,7 @@ const Navbar = class extends React.Component {
             </span>
           </OutboundLink>
           <OutboundLink
-            className="navbar-item is-hidden-desktop"
+            className="navbar-item is-hidden-desktop-only"
             href="https://twitter.com/AbhithRajan"
             target="_blank"
           >
@@ -77,108 +145,35 @@ const Navbar = class extends React.Component {
               <FaTwitter size={24} />
             </span>
           </OutboundLink>
-          <div
-            role="button"
-            className={`navbar-burger burger ${this.state.hamburgerMenuClass}`}
-            onClick={() => this.toggleHamburger()}
-            onKeyDown={() => this.toggleHamburger()}
-            aria-label="menu"
-            aria-expanded="false"
-            data-target="ar-navMenu"
-            tabIndex={0}
-          >
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </div>
-        </div>
-        <div
-          id="ar-navMenu"
-          className={`navbar-menu ${this.state.navBarActiveClass}`}
-        >
-          <div className="navbar-start">
-            <NavbarItem to="/" title="Home">
-              <FcHome />
-            </NavbarItem>
-            <NavbarItem to="/about/" title="About">
-              <FcPortraitMode />
-            </NavbarItem>
-            <NavbarItem to="/blog/" title="Blog">
-              <FcReading />
-            </NavbarItem>
-            <NavbarItem to="/snippets/" title="Snippets">
-              <FcPuzzle />
-            </NavbarItem>
-            <NavbarItem to="/recommended/" title="Recommended">
-              <FcLike />
-            </NavbarItem>
-            <NavbarItem to="/topics/" title="Topics">
-              <FcTimeline />
-            </NavbarItem>
-            <NavbarItem to="/contact/" title="Contact">
-              <FcContacts />
-            </NavbarItem>
-          </div>
-          <div className="navbar-end">
-            <div className="navbar-item is-hidden-desktop-only is-hidden-widescreen-only">
-              <div className="field is-grouped">
-                <iframe
-                  src="https://ghbtns.com/github-btn.html?user=Abhith&repo=abhith.net&type=star&count=true&size=large"
-                  frameBorder="0"
-                  scrolling="0"
-                  width="170"
-                  height="30"
-                  title="Star Abhith/abhith.net on GitHub"
-                ></iframe>
-              </div>
-            </div>
-            <OutboundLink
-              className="navbar-item is-hidden-desktop-only"
-              href="https://github.com/Abhith/abhith.net"
-              target="_blank"
-            >
-              <span className="icon">
-                <GoMarkGithub size={24} />
-              </span>
-            </OutboundLink>
-            <OutboundLink
-              className="navbar-item is-hidden-desktop-only"
-              href="https://twitter.com/AbhithRajan"
-              target="_blank"
-            >
-              <span className="icon" style={{ color: "#55acee" }}>
-                <FaTwitter size={24} />
-              </span>
-            </OutboundLink>
-            <div className="navbar-item">
-              <div className="field is-grouped">
-                <Link
-                  to="/donate/"
-                  className="button k-button k-primary raised has-gradient slanted"
-                >
-                  <span>
-                    BUY ME A <FaCoffee />
-                  </span>
-                </Link>
-              </div>
+          <div className="navbar-item">
+            <div className="field is-grouped">
+              <Link
+                to="/donate/"
+                className="button k-button k-primary raised has-gradient slanted"
+              >
+                <span>
+                  BUY ME A <FaCoffee />
+                </span>
+              </Link>
             </div>
           </div>
         </div>
-      </nav>
-    );
-  }
-};
+      </div>
+    </nav>
+  );
+}
 
 export default () => (
   <StaticQuery
-    query={graphql`{
-  logo: file(relativePath: {eq: "abhith-logo-lg.png"}) {
-    childImageSharp {
-      gatsbyImageData(layout: FULL_WIDTH)
-    }
-  }
-}
-`}
+    query={graphql`
+      {
+        logo: file(relativePath: { eq: "abhith-logo-lg.png" }) {
+          childImageSharp {
+            gatsbyImageData(layout: FULL_WIDTH)
+          }
+        }
+      }
+    `}
     render={(data) => <Navbar logo={data.logo} />}
   />
 );
