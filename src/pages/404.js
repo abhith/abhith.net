6aaf69341089176d50105e65610d883104653251
Abhith Rajan <dearabhith@gmail.com>
2022-07-30 20:42:01 +0400

    fix: 'class' is not a valid prop for React DOM element

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,18 +6,18 @@ import { OutboundLink } from "gatsby-plugin-google-gtag";
 
 const NotFoundPage = () => (
   <Layout>
-    <section class="section is-medium">
-      <div class="container">
-        <div class="columns is-vcentered">
-          <div class="column has-text-centered">
-            <h1 class="title">404 Page Not Found</h1>
-            <p class="subtitle">
+    <section className="section is-medium">
+      <div className="container">
+        <div className="columns is-vcentered">
+          <div className="column has-text-centered">
+            <h1 className="title">404 Page Not Found</h1>
+            <p className="subtitle">
               The page you are looking for doesn't exist or an unexpected error
               has occurred 😞
               <br /> Do drop me a line about the missing page if you have time
               👍
             </p>
-            <div class="buttons are-large is-centered">
+            <div className="buttons are-large is-centered">
               <Link className="button is-primary is-light" to="/">
                 Home
               </Link>
@@ -36,7 +36,7 @@ const NotFoundPage = () => (
               </Link>
             </div>
           </div>
-          <div class="column has-text-centered">
+          <div className="column has-text-centered">
             <img
               src="/img/site/404.jpg"
               alt="404 Page Not Found"
