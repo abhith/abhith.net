fbf05e05bf5d4021605e1682ad68955d0e623919
Abhith Rajan <dearabhith@gmail.com>
2022-08-10 21:43:57 +0400

    style: dark mode support on github typo component

--- src/components/github-typo.js
+++ src/components/github-typo.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { OutboundLink } from "gatsby-plugin-google-gtag";
+import { useColorMode } from "theme-ui";
 
 const GitHubTypo = ({ githubURL }) => {
+  const [colorMode] = useColorMode();
+  const className = `${
+    colorMode === "light" ? "has-text-grey" : "has-text-grey-dark"
+  }`;
   return (
     <div id="typo" className="ar-typo">
-      <p className="has-text-grey">
-        This page is <strong className="has-text-grey">open source</strong>.
-        Noticed a typo? Or something unclear?
+      <p className={className}>
+        This page is <strong className={className}>open source</strong>. Noticed
+        a typo? Or something unclear?
         <br />
-        <OutboundLink
-          href={githubURL}
-          target="_blank"
-          className="has-text-grey"
-        >
+        <OutboundLink href={githubURL} target="_blank" className={className}>
           Improve this page on GitHub
         </OutboundLink>
       </p>
