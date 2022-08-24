32b3174a32a99f4cf724aa234f37e32344a91e03
Abhith Rajan <dearabhith@gmail.com>
2022-08-24 21:42:46 +0400

    chore: add GA4 property

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,7 +42,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: ["UA-104139048-1"],
+        trackingIds: ["UA-104139048-1", "G-3JV7V7YBHL"],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
         gtagConfig: {
