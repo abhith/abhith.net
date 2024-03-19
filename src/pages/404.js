5b5e136a16405571f6c839d2f6d9f8492b84489d
Abhith <dearabhith@gmail.com>
2024-03-20 00:19:17 +0400

    fix: github icon on 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Layout from "../components/layout";
 import { Link } from "gatsby";
-import { GoMarkGithub } from "react-icons/go";
+import { FaGithub } from "react-icons/fa";
 import { OutboundLink } from "gatsby-plugin-google-gtag";
 
 const NotFoundPage = () => (
@@ -27,7 +27,7 @@ const NotFoundPage = () => (
                 target="_blank"
               >
                 <span className="icon">
-                  <GoMarkGithub />
+                  <FaGithub />
                 </span>
                 <span></span>
               </OutboundLink>
